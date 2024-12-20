51c98df7377b8bcc624d4282161980f5a03a50eb
HOSTI1315 <githubmoment@gmail.com>
2024-12-20 21:03:07 +0300

    Update autoexe.lua

--- autoexe.lua
+++ autoexe.lua
@@ -6,13 +6,14 @@ task.wait( math.random() )
 
 local games = {
 	[4386104527] = 'https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader', -- AS
+	[89438510123061] = 'https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader', -- AS
 	[5292947] =  'https://api.luarmor.net/files/v3/loaders/eef1f2c7ccd0aaf83d95f052ef2e934a.lua'
 	
 }
 
 if games[game.CreatorId] then
 	task.wait( math.random() )
-	if game.CreatorId == 4386104527 or game.CreatorId == 12229756 or game.CreatorId == 15762744 or game.CreatorId == 33859442 then
+	if game.CreatorId == 4386104527 or game.CreatorId == 89438510123061 or game.CreatorId == 15762744 or game.CreatorId == 33859442 then
 		print('Loaded')
 		repeat 
 			loadstring(game:HttpGet(games[game.CreatorId]))()
