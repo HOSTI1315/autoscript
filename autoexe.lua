3f6897923053eaa2b8adfd173f833d6808f721fc
HOSTI1315 <githubmoment@gmail.com>
2024-12-20 21:01:40 +0300

    Update autoexe.lua

--- autoexe.lua
+++ autoexe.lua
@@ -6,7 +6,7 @@ task.wait( math.random() )
 
 local games = {
 	[4386104527] = 'https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader', -- AS
-	[5292947] =  'https://api.luarmor.net/files/v3/loaders/eef1f2c7ccd0aaf83d95f052ef2e934a.lua',
+	[5292947] =  'https://api.luarmor.net/files/v3/loaders/eef1f2c7ccd0aaf83d95f052ef2e934a.lua'
 	
 }
 
