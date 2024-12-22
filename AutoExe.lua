7e7b40283ff62cbafe8c0991c4bf8ac427b09fdf
HOSTI1315 <githubmoment@gmail.com>
2024-12-22 22:03:07 +0300

    Update AutoExe.lua

--- AutoExe.lua
+++ AutoExe.lua
@@ -1,43 +1,52 @@
--- Определение таблицы с CreatorId и URL для загрузки скриптов
-local scripts = {
-   --- CreatorID
-	-- Фиш
-	[7381705] = "https://nousigi.com/loader.lua",  -- Замените на реальные ссылки
-	-- АС
-	[33781793] = "https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader",
-	-- АР
-	[33820338] = "https://nousigi.com/loader.lua",
-	-- Шары
-	[32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"
+-- Глобальная переменная для отслеживания выполнения скрипта
+if not getgenv().scriptExecuted then
+
+    -- Определение таблицы с CreatorId и URL для загрузки скриптов
+    local scripts = {
+     --- CreatorID
+	    -- Фиш
+	    [7381705] = "https://nousigi.com/loader.lua",  -- Замените на реальные ссылки
+	    -- АС
+	    [33781793] = "https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader",
+	    -- АР
+	    [33820338] = "https://nousigi.com/loader.lua",
+	    -- Шары
+	    [32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"
 	
-}
+    }
 
--- Функция для загрузки и выполнения скрипта по URL
-local function loadAndRunScript(url)
-    local success, script = pcall(function()
-        return game:HttpGet(url)
-    end)
+    -- Функция для загрузки и выполнения скрипта по URL
+    local function loadAndRunScript(url)
+        local success, script = pcall(function()
+            return game:HttpGet(url)
+        end)
     
-    if success then
-        -- Выполнение скрипта
-        loadstring(script)()
-    else
-        warn("Ошибка при загрузке скрипта: " .. script)
+        if success then
+            -- Выполнение скрипта
+            loadstring(script)()
+        else
+           warn("Ошибка при загрузке скрипта: " .. script)
+        end
     end
-end
 
--- Основной блок для проверки текущего CreatorId и выполнения соответствующего скрипта
-local function runScriptForCreator()
-    local currentCreatorId = game.CreatorId
-    local scriptUrl = scripts[currentCreatorId]
+    -- Основной блок для проверки текущего CreatorId и выполнения соответствующего скрипта
+    local function runScriptForCreator()
+     local currentCreatorId = game.CreatorId
+     local scriptUrl = scripts[currentCreatorId]
 
-    if scriptUrl then
-        -- Если для данного CreatorId есть соответствующий скрипт, загружаем и выполняем его
-        loadAndRunScript(scriptUrl)
-    else
-        warn("Для данного CreatorId (" .. currentCreatorId .. ") не найдено подходящего скрипта.")
+     if scriptUrl then
+            -- Если для данного CreatorId есть соответствующий скрипт, загружаем и выполняем его
+            loadAndRunScript(scriptUrl)
+        else
+          warn("Для данного CreatorId (" .. currentCreatorId .. ") не найдено подходящего скрипта.")
+        end
     end
-end
 
--- Запуск основной функции
-runScriptForCreator()
+    -- Запуск основной функции
+    runScriptForCreator()
+
+    -- Устанавливаем флаг, чтобы избежать повторного запуска
+    getgenv().scriptExecuted = true
+else
+    warn("Скрипт уже был выполнен.")
+end
