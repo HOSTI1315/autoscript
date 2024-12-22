e76516d59562f0d787743a7d6f53d8bdbe17e01f
HOSTI1315 <githubmoment@gmail.com>
2024-12-22 22:06:10 +0300

    Update AutoExe.lua

--- AutoExe.lua
+++ AutoExe.lua
@@ -1,18 +1,16 @@
 -- Глобальная переменная для отслеживания выполнения скрипта
 if not getgenv().scriptExecuted then
-
     -- Определение таблицы с CreatorId и URL для загрузки скриптов
     local scripts = {
-     --- CreatorID
-	    -- Фиш
-	    [7381705] = "https://nousigi.com/loader.lua",  -- Замените на реальные ссылки
-	    -- АС
-	    [33781793] = "https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader",
-	    -- АР
-	    [33820338] = "https://nousigi.com/loader.lua",
-	    -- Шары
-	    [32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"
-	
+        --- CreatorID
+        -- Фиш
+        [7381705] = "https://nousigi.com/loader.lua", 
+        -- АС
+        [33781793] = "https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader",
+        -- АР
+        [33820338] = "https://nousigi.com/loader.lua",
+        -- Шары
+        [32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"
     }
 
     -- Функция для загрузки и выполнения скрипта по URL
@@ -20,25 +18,29 @@ if not getgenv().scriptExecuted then
         local success, script = pcall(function()
             return game:HttpGet(url)
         end)
-    
+        
         if success then
             -- Выполнение скрипта
             loadstring(script)()
         else
-           warn("Ошибка при загрузке скрипта: " .. script)
+            warn("Ошибка при загрузке скрипта: " .. script)
         end
     end
 
     -- Основной блок для проверки текущего CreatorId и выполнения соответствующего скрипта
     local function runScriptForCreator()
-     local currentCreatorId = game.CreatorId
-     local scriptUrl = scripts[currentCreatorId]
+        -- Дожидаемся, пока игра полностью загрузится
+        game.Loaded:Wait()
+        
+        -- Получаем текущий CreatorId
+        local currentCreatorId = game.CreatorId
+        local scriptUrl = scripts[currentCreatorId]
 
-     if scriptUrl then
+        if scriptUrl then
             -- Если для данного CreatorId есть соответствующий скрипт, загружаем и выполняем его
             loadAndRunScript(scriptUrl)
         else
-          warn("Для данного CreatorId (" .. currentCreatorId .. ") не найдено подходящего скрипта.")
+            warn("Для данного CreatorId (" .. currentCreatorId .. ") не найдено подходящего скрипта.")
         end
     end
 
