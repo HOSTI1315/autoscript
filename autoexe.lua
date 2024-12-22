4d55e5da465be7a939aa5b5249205748c5c43a16
HOSTI1315 <githubmoment@gmail.com>
2024-12-22 20:28:23 +0300

    Update autoexe.lua

--- autoexe.lua
+++ autoexe.lua
@@ -1,3 +1,6 @@
-loadstring(game:HttpGet("https://nousigi.com/loader.lua"))(),
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader"))(),
-loadstring(game:HttpGet("https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"))()
+local skrilya = {
+    [16732694052] = 'https://nousigi.com/loader.lua',
+    [89438510123061] = 'https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader',
+    [16841929299] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua',
+}   
+loadstring(game:HttpGet(skrilya[game.PlaceId]))()
