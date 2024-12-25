a60db6539adf0e281ef82cbffd1a22c238f97812
HOSTI1315 <githubmoment@gmail.com>
2024-12-25 19:35:20 +0300

    Update AutoExe.lua

--- AutoExe.lua
+++ AutoExe.lua
@@ -11,10 +11,10 @@ if not getgenv().scriptExecuted then
         [33820338] = "https://nousigi.com/loader.lua",
         -- Flop
         [33799733] = "https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader",
-        -- Шары
-        [32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua",
         -- АА
-        [10611639] = "https://nousigi.com/loader.lua"
+        [10611639] = "https://nousigi.com/loader.lua", 
+        -- Шары
+        [32380537] = "https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua"
     }
 
     -- Функция для загрузки и выполнения скрипта по URL
