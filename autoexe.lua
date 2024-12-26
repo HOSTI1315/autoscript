b705b70c13b2d2e9eb5f2b6f32d82207ef295295
HOSTI1315 <githubmoment@gmail.com>
2024-12-26 21:38:27 +0300

    Update and rename autoexe.lua to autofind.lua

--- autoexe.lua
+++
@@ -1,6 +0,0 @@
-local skrilya = {
-    [16732694052] = 'https://nousigi.com/loader.lua',
-    [89438510123061] = 'https://raw.githubusercontent.com/ZaRdoOx/Fazium-files/main/Loader',
-    [16841929299] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua',
-}   
-loadstring(game:HttpGet(skrilya[game.PlaceId]))()
