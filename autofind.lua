58a5c0d2e065e0525e27422bc2a2fede103c2c51
HOSTI1315 <githubmoment@gmail.com>
2024-12-27 17:02:03 +0300

    Update autofind.lua

--- autofind.lua
+++ autofind.lua
@@ -2,4 +2,12 @@ local args = {
     [1] = "christmas_event"
 }
 
-game:GetService("ReplicatedStorage").endpoints.client_to_server.request_matchmaking:InvokeServer(unpack(args))
+coroutine.resume(coroutine.create(function()
+    wait(5) -- Задержка перед запуском скрипта на 5 секунд
+    game:GetService("ReplicatedStorage").endpoints.client_to_server.request_matchmaking:InvokeServer(unpack(args))
+    
+    while true do
+        wait(10) -- Повторять скрипт каждые 10 секунд
+        game:GetService("ReplicatedStorage").endpoints.client_to_server.request_matchmaking:InvokeServer(unpack(args))
+    end
+end))
