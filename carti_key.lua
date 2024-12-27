7a42eeb6ec9c4397f6589ec1a508678ac301c615
HOSTI1315 <githubmoment@gmail.com>
2024-12-27 17:27:20 +0300

    Update carti_key.lua

--- carti_key.lua
+++ carti_key.lua
@@ -1,2 +1,2 @@
-script_key="kxbgmrXSxYYcjhtwESDAiXOjgQkBbUAz";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/485daf6c6c96201027b51ae694a508d0.lua"))()
+script_key="ypKQKqbfNZwvPZcBJiNJaemGReUwMERt";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/buang5516/buanghub/main/PremiumBuangHub.lua"))()
