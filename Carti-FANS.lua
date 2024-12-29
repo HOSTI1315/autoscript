a8cc3950938a614778ca6ca1dcac3cd82a87859e
HOSTI1315 <githubmoment@gmail.com>
2024-12-29 22:55:45 +0300

    Update Carti-FANS.lua

--- Carti-FANS.lua
+++ Carti-FANS.lua
@@ -1,2 +1,5 @@
-script_key="DOyoyepiWosFTsXABVGKywIPtreuPHEf";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/485daf6c6c96201027b51ae694a508d0.lua"))()
+getgenv().SmallUI = false -- set to true if you want a smaller GUI
+getgenv().noopenanim = true -- disables the opening animation
+getgenv().hidegui = true -- hides UI after execute
+script_key="govSyarHSMYuKtNJoLFGElWELgGWVXmz";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
