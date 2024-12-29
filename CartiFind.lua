12779042c3d1686b87a357f4703537b46da1938c
HOSTI1315 <githubmoment@gmail.com>
2024-12-29 23:13:10 +0300

    Add files via upload

---
+++ CartiFind.lua
@@ -0,0 +1,5 @@
+getgenv().SmallUI = false -- set to true if you want a smaller GUI
+getgenv().noopenanim = true -- disables the opening animation
+getgenv().hidegui = true -- hides UI after execute
+script_key="CzBCsgZzXNqcpvDXHuwiGfWBNCdUOFwe";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
