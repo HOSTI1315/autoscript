43bb584fd44dcbd1c8a94bc50b453dff798c2557
HOSTI1315 <githubmoment@gmail.com>
2025-01-01 19:23:53 +0300

    Update CartiBetaV2.lua

--- CartiBetaV2.lua
+++ CartiBetaV2.lua
@@ -1,2 +1,20 @@
-script_key="kxbgmrXSxYYcjhtwESDAiXOjgQkBbUAz";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/485daf6c6c96201027b51ae694a508d0.lua"))()
+local function loadScript()
+    local success, errorMessage = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HOSTI1315/autoscript/refs/heads/main/CartiBeta.lua"))()
+    end)
+
+    if not success then
+        warn("Ошибка при загрузке скрипта: " .. errorMessage)
+        return false
+    end
+    return true
+end
+
+-- Попытка загрузить скрипт, пока он не загрузится успешно
+local scriptLoaded = false
+while not scriptLoaded do
+    scriptLoaded = loadScript()
+    if not scriptLoaded then
+        wait(2)  -- Пауза перед новой попыткой загрузки
+    end
+end
