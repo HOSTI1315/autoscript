291d2ec3333371a93b8089a020cabdbdb783b188
HOSTI1315 <githubmoment@gmail.com>
2025-02-22 01:49:08 +0300

    Update BF.lua

--- BF.lua
+++ BF.lua
@@ -1,41 +1,5 @@
 script_key = "iOBQIRwEZvnsKtZaGrYWPhbTmqYQXoFI"
-getgenv().FpsCap = 10
-getgenv().OneClickUi = true -- Only Open Necessacry Ui For One Click
-getgenv().FpsBoost1 = false
-    getgenv().WhiteScreen = true
-    getgenv().UiCheckItems = false
-    getgenv().OneClickSetting = {
-        Enable=true,
-        UnlimitGetQuest=true,
-        TripleQuest=true,
-        AutoAddStats=true,
-        RedeemCode=true,
-        Sea2KeyHop=true,
-        FruitEat = { --Priority, Name, 
-            [1] = {"Magma-Magma"},
-            [2] = {"Magma-Magma"}
-        },
-        EatFruitFromStorage = true,
-        SnipeFruit = true,
-        SnipeFruitMirage = true,
-        HopIfFoundNearExploiter = false,
-        HopHakiColor =false,
-        HopTushita = false,
-        HopValkyriehelm = true,
-        HopMirrorFractal=true,
-        FarmPole = false, -- Turn off If Want Focus Level
-        FarmItems = false, --Turn off If Want Focus Level And CDK, THis Only get After You Get God Human
-        DisableSoulGuitar = false,
-        DisableCDK = false,
-         RollRace={
-            Enable = false,
-            Races = {"Mink","Fishman","Human","Skypiea"}
-        },
-               Mastery = false,
-        SwanFruitHop=false
-    }
-    getgenv().OneClickFarms = {
-        ["Shark Anchor"] = false,
-    }
 getgenv().Team = "Pirates"
-getgenv().AutoLoad = false --Will Load Script On Server Hop
+getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
