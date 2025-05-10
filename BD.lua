f5756c76d3a349146430c5424a7b13724a99bca7
HOSTI1315 <githubmoment@gmail.com>
2025-05-10 17:16:14 +0300

    Update BD.lua

--- BD.lua
+++ BD.lua
@@ -1,13 +1,5 @@
--- Получаем Service
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+wait(25)
 
--- Ждём, пока в ReplicatedStorage не появится объект "Client"
--- (если вам нужно ждать какой‑то другой объект — замените "Client" на его имя)
-local clientFolder = ReplicatedStorage:WaitForChild("Client")
 
--- Можете при желании проверить, что это действительно нужный объект
--- print("Client найден:", clientFolder and true or false)
-
--- Теперь можно безопасно запускать скрипт‑загрузчик
 local script_key = "ZpfbhzvLcOxkjaSdLDgTUALbfKEHerfr"
 loadstring(game:HttpGet("https://zekehub.com/scripts/Loader.lua"))()
