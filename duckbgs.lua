ecb6b1003cb51c69109777641c49fb9ab844c3dc
HOSTI1315 <githubmoment@gmail.com>
2025-05-12 23:50:49 +0300

    Update duckbgs.lua

--- duckbgs.lua
+++ duckbgs.lua
@@ -123,4 +123,4 @@ getgenv().UserConfig = {
 }
 
 -- Load the script engine
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0cf205a620e81d1f3c3d3f75474ab32f.lua "))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0cf205a620e81d1f3c3d3f75474ab32f.lua"))()
