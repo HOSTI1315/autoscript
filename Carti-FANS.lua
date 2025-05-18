55e8b5592e3ec71dbd1127a76b02b6c65e2001e3
HOSTI1315 <githubmoment@gmail.com>
2025-05-18 12:48:59 +0300

    Delete Carti-FANS.lua

--- Carti-FANS.lua
+++
@@ -1,5 +0,0 @@
-getgenv().SmallUI = false -- set to true if you want a smaller GUI
-getgenv().noopenanim = true -- disables the opening animation
-getgenv().hidegui = true -- hides UI after execute
-script_key="govSyarHSMYuKtNJoLFGElWELgGWVXmz";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
